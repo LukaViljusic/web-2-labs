a669e65504319bbacc874fe425027e6c4741e419
LukaViljusic <lukaviljusic@gmail.com>
2025-10-25 15:04:04 +0200

    Update client setup

--- lab1/src/client.ts
+++ lab1/src/client.ts
@@ -1,12 +1,15 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
-import https from 'https'
-import fs from 'fs'
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import https from 'https';
+import fs from 'fs';
+import cors from 'cors';
 import { auth, requiresAuth } from 'express-openid-connect';
+import { auth as auth2 } from 'express-oauth2-jwt-bearer';
 import { insertTicket, getLotoNumbersById, getResultByRoundId, 
-  getTicketsCount, getLastRoundId } from './database';
-import QRCode from 'qrcode'
+  getTicketsCount, getLastRoundId, openNewRound, 
+  closeRound, insertResultNumbersManual, getOpenRoundId } from './database';
+import QRCode from 'qrcode';
 
 dotenv.config();
 const app = express();
@@ -16,6 +19,9 @@ app.set("view engine", "pug");
 const externalUrl = process.env.RENDER_EXTERNAL_URL;
 const port = process.env.PORT ? parseInt(process.env.PORT) : 4010;
 const hostname = externalUrl ? '0.0.0.0' : '127.0.0.1';
+
+app.use(cors());
+app.use(express.json());
 
 const config = {
     authRequired: false,
@@ -27,7 +33,12 @@ const config = {
 };
 
 app.use(auth(config));
-app.use(express.json());
+
+const jwtCheck = auth2({
+  audience: 'https://web2loto/api',
+  issuerBaseURL: 'https://dev-pu5ikuuzt4xlqtzg.us.auth0.com/',
+});
+
 
 app.get('/', async (req, res) => {  
     const ticketCount = await getTicketsCount();
@@ -76,6 +87,79 @@ app.get('/loto-numbers/:uuid', async (req, res) => {
   const resultNums = await getResultByRoundId(lotoTicket.round_id);
 
   res.render("lotonums",  {lotoNums: lotoTicket.numbers, resultNums: resultNums});
+});
+
+
+app.post('/new-round', jwtCheck, async function(req, res) {
+  try {
+    const result = await openNewRound();
+    if(result) {
+      res.status(200);
+      res.end("Uspješno otvorena nova runda!");
+    } else {
+      res.status(204);
+      res.end("Runda je već otvorena.");
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(500);
+    res.end('Greška kod otvaranja novog kola.');
+  }
+})
+
+app.post('/close', jwtCheck, async function(req, res) {
+  try {
+    const result = await closeRound();
+    if(result) {
+      res.status(200);
+      res.end("Uspješno zatvaranje runde");
+    } else {
+      res.status(204);
+      res.end("Nema aktivnih rundi");
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(500);
+    res.end('Greška kod zatvaranja kola.');
+  }
+})
+
+app.post('/store-results', jwtCheck, async function(req, res) {
+  try {
+    const { numbers }: { numbers: number[] } = req.body;
+
+    if (!numbers || numbers.length === 0) {
+      res.status(400).end("Nije poslano ispravno polje brojeva");
+      return;
+    }
+
+    const lastRoundId = await getLastRoundId();
+    if (lastRoundId === 0) {
+      res.status(400).end("Nema evidentiranih kola u bazi.");
+      return;
+    }
+
+    const openRoundId = await getOpenRoundId();
+    if (openRoundId !== 0) {
+      res.status(400).end("Uplate su još aktivne!");
+      return;
+    }
+
+    const existingResults = await getResultByRoundId(lastRoundId);
+    if (existingResults !== null) {
+      res.status(400).end("Rezultati ovog kola već su zabilježeni.");
+      return;
+    }
+
+    await insertResultNumbersManual(lastRoundId, numbers);
+
+    res.status(204)
+    res.end("Uspješno spremanje rezultata!");
+
+  } catch(err) {
+    console.error('Greška kod spremanja rezultata kola:', err);
+    res.status(500).end('Greška kod spremanja rezultata kola.');
+  }
 });
 
 
